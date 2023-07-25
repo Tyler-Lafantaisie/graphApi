763a77327803ddc66611d76869ad5b98bbb944f7
Tyler Lafantaisie <tylerlafantaisie@gmail.com>
2023-07-25 09:33:48 -0400

    file set up

---
+++ src/app.ts
@@ -0,0 +1,4 @@
+function cookies() {
+
+let thing:string = "top"
+}
---
+++ src/scrape/scraper.ts
@@ -0,0 +1,8 @@
+// scrape data with axiso and out put the data as a csv file. scraping https://en.wikipedia.org/wiki/List_of_mass_shootings_in_the_United_States. for mass shooting data.
+
+const h:string = "hello";
+
+let w:string = "world";
+
+
+console.log(h+w);
