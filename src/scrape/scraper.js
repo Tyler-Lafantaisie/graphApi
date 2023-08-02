5ad17476ca61bc31cbd314b6549ab4094b159372
Tyler Lafantaisie <tylerlafantaisie@gmail.com>
2023-08-02 07:34:43 -0400

    finished making the scraper

--- src/scrape/scraper.js
+++ src/scrape/scraper.js
@@ -1,24 +1,22 @@
 // scrape data with axiso and out put the data as a csv file. scraping https://en.wikipedia.org/wiki/List_of_mass_shootings_in_the_United_States. for mass shooting data.
-
 // imports
 const axios = require("axios");
 const fs = require("fs");
 const cheerio = require("cheerio");
 //code
-
-const fileName:string = "ShootingData.json";
-const link:string = "https://en.wikipedia.org/wiki/List_of_mass_shootings_in_the_United_States";
+const fileName = "ShootingData.json";
+const link = "https://en.wikipedia.org/wiki/List_of_mass_shootings_in_the_United_States";
 async function Scraper() {
     try {
         const scrapingData = await axios.get(link);
         const html = scrapingData.data;
         const $ = cheerio.load(html);
         const textData = $('div').text();
-        const jsonData = JSON.stringify(textData,null,2);
+        const jsonData = JSON.stringify(textData, null, 2);
         fs.writeFileSync(fileName, jsonData);
-    }catch (error){
+    }
+    catch (error) {
         console.error(error);
-
     }
 }
 // Read both axio docs and cheerio docs to find a way to set data up. put data in json file with fs.WriteFileSync
